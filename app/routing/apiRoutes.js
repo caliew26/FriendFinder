eda91c8486433ed6933fe668c0d95469e6732c25
Adventure 2019 <caliew26@gmail.com>
2019-06-22 16:42:27 -0400

    fixing the survey form and submit, is not complete yet, just working to get the submit button working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,38 +4,38 @@ var friendsData = require ("../data/friends");
 
 module.exports = function(app){
     app.get("/survey.html",function(req,res){
-        console.log("survey route");
+        // console.log("survey route");
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     })
 
     app.get("/api/friends",function(req,res){
         res.json(friendsData);
         console.log(friendsData);
-    // instructions: This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
     });
 
+    app.get("/add", function(request,response){
+        console.log("this is the body " + JSON.stringify(request.body));
+        var result = request.body["num1"]+request.body["num2"]
+        response.send(result.toString());
+    })
+
     app.post("/api/friends", function(req, res){
-        //this is where the calculation is going to happen, I'm not sure how this is going to happen
+        console.log("this is the post api/friends");
+        //this is where the calculation is going to happen
         
         //get the userQues from the survey.html file
         var userQues = req.body;
+        console.log("this is the req.body " + JSON.stringify(req.body));
         //get the friendsGroup from the friends.js file
         //compare index to index between the 2 files
-//above 3 lines would need to be a forloop??
+        //above 3 lines would need to be a forloop??
 
         //the value that matches closest to each other is the new friend
         //in the modal
             //show the friend name
-        var matchFriendName = friendsGroup.name;
-            //show the friend picture
-        var matchFriendPic = friendsGroup.picture;
-
-        for(let i = 0; i < friendsData.length; i++){
-            var difference = 0;
-            for (let j = 0; j < userQues.length; j++){
-                difference += Math.abs(friendsData[i].score[j] - userQues[j])
-            }
-            console.log(difference);
-        }
+        // var matchFriendName = friendsGroup.name;
+        //     //show the friend picture
+        // var matchFriendPic = friendsGroup.picture;
+        res.send("here's your response");
     })
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -11,7 +11,14 @@ module.exports = function(app){
 
     //this is the "survey" page - the user will be directed to the survey page when the /survey is called
     app.get("/survey.html",function(req,res){
-        console.log("survey route");
+        // console.log("survey route");
         res.sendFile(path.join(__dirname, "../pubic/survey.html"));
+    })
+
+    app.get("/data", function(req,res){
+        // console.log(res + " this is res");
+        // console.log(req + "this is req");
+        res.sendFile(path.join(__dirname, "../data/friends.json"))
+        // res.send("You got this!")
     })
 }
